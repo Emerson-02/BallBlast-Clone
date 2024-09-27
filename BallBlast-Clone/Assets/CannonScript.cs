ae8fec71fe46c04589ccd655a08ba591841f05ec
Emerson-02 <69188698+Emerson-02@users.noreply.github.com>
2024-09-27 06:06:54 -0300

    Jewels and coins

--- BallBlast-Clone/Assets/CannonScript.cs
+++ BallBlast-Clone/Assets/CannonScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 public class CannonScript : MonoBehaviour
 {
@@ -14,6 +16,19 @@ public class CannonScript : MonoBehaviour
 
     public bool isDragging = false;
     private Vector3 lastMousePosition;
+
+    public bool canMove = true;
+
+    public JewelSpawner jewelSpawner;
+    public PlayerStats playerStats;
+    public UIController uiController;
+
+    private void Awake() 
+    {
+        uiController = FindObjectOfType<UIController>();
+        playerStats = FindObjectOfType<PlayerStats>();
+        jewelSpawner = FindObjectOfType<JewelSpawner>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -30,38 +45,89 @@ public class CannonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(canMove)
         {
-            isDragging = true;
-            lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        }
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
 
-        if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+            }
+
+            if (isDragging)
+            {
+                Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 movement = currentMousePosition - lastMousePosition;
+                movement.z = 0; // Garantir que o movimento seja apenas no plano XY
+                movement.y = 0; // Ignorar o movimento no eixo Y
+
+                // Limitar a posição do canhão dentro dos limites da tela ajustados
+                Vector3 newPosition = transform.position + movement;
+                newPosition.x = Mathf.Clamp(newPosition.x, screenLeftLimit + halfCannonWidth, screenRightLimit - halfCannonWidth);
+                movement = newPosition - transform.position; // Atualizar o movimento com base na posição limitada
+                transform.position = newPosition;
+
+                // Calcular a rotação das rodas
+                float wheelRotation = movement.x * (wheelRotationSpeed/2);
+
+                // Aplicar a rotação às rodas
+                wheelLeft.Rotate(Vector3.forward, -wheelRotation);
+                WheelRight.Rotate(Vector3.forward, -wheelRotation);
+
+                lastMousePosition = currentMousePosition;
+            }
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Jewel"))
         {
+            jewelSpawner.StopSpawning();
+
+            canMove = false;
             isDragging = false;
-        }
 
-        if (isDragging)
+            // pega todos os objetos com a tag Jewel na cena
+            GameObject[] jewels = GameObject.FindGameObjectsWithTag("Jewel");
+
+            // para cada objeto Jewel na cena
+            foreach (GameObject jewel in jewels)
+            {
+                // pega o componente Rigidbody2D do objeto Jewel
+                Rigidbody2D rb = jewel.GetComponent<Rigidbody2D>();
+
+                // muda para kinematic
+                rb.bodyType = RigidbodyType2D.Kinematic;
+
+                // para o objeto Jewel
+                rb.velocity = Vector2.zero;
+
+                // para a rotação do objeto Jewel
+                rb.angularVelocity = 0;
+
+            }
+
+
+
+            // FlashRed
+            StartCoroutine(other.gameObject.GetComponent<Jewel>().FlashRed());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Coin"))
         {
-            Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 movement = currentMousePosition - lastMousePosition;
-            movement.z = 0; // Garantir que o movimento seja apenas no plano XY
-            movement.y = 0; // Ignorar o movimento no eixo Y
-
-            // Limitar a posição do canhão dentro dos limites da tela ajustados
-            Vector3 newPosition = transform.position + movement;
-            newPosition.x = Mathf.Clamp(newPosition.x, screenLeftLimit + halfCannonWidth, screenRightLimit - halfCannonWidth);
-            movement = newPosition - transform.position; // Atualizar o movimento com base na posição limitada
-            transform.position = newPosition;
-
-            // Calcular a rotação das rodas
-            float wheelRotation = movement.x * (wheelRotationSpeed/2);
-
-            // Aplicar a rotação às rodas
-            wheelLeft.Rotate(Vector3.forward, -wheelRotation);
-            WheelRight.Rotate(Vector3.forward, -wheelRotation);
-
-            lastMousePosition = currentMousePosition;
+            playerStats.coins++;
+            uiController.UpdateCoins(playerStats.coins);
+            Destroy(other.gameObject);
+            
         }
+       
     }
 }
--- BallBlast-Clone/Assets/PlayerStats.cs
+++ BallBlast-Clone/Assets/PlayerStats.cs
@@ -7,7 +7,6 @@ public class PlayerStats : MonoBehaviour
     public float bulletInterval = 0.5f; // Intervalo entre as balas em segundos
     public int bulletsPerShot = 1; // Número de balas disparadas de uma vez
     public float bulletSpeed = 10f; // Velocidade das balas
-    public int playerHealth = 100; // Saúde do jogador
-
     public int damage = 1; // Dano causado pelas balas
+    public int coins = 0; // Moedas coletadas
 }
